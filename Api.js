418e3b0b44db57966f3560224114bb6829306b32
VladimirGjurov <vladimir.gjurov@outlook.com>
2025-08-20 20:04:26 +1000

    Testing

--- Api.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-
-const url = 'https://evilinsult.com/generate_insult.php?lang=en&type=json';
-
-fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
